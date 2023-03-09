bba0f45e2378b77e3ad70b36db522de316b6a4fc
Amandalfs <caduc57@gmail.com>
2023-03-09 15:34:55 -0300

    feat
    Adicionado a parte de retornar o carrosel

--- assets/javascript/carrosel.js
+++ assets/javascript/carrosel.js
@@ -74,8 +74,71 @@ btnAvancar.addEventListener('click', ()=>{
     carroselAvancar();
 })
 
-/* setInterval(carroselAvancar, 2000);
- */
+
+
+setInterval(carroselAvancar, 3000);
+
+
 function carroselRetorcer(){
 
+    const elementoCompletoEsquerda = document.querySelector('.carrosel_direta');
+    const linkEsquerda = document.querySelector('.esquerda_link');
+    const imagemEsquerda = document.querySelector('.esquerda_imagem');
+
+    const elementoCompletoMeio = document.querySelector('.carrosel_meio');
+    const linkMeio = document.querySelector('.meio_link');
+    const descricaoMeio = document.querySelector('.descricao_meio');
+    const tituloMeio = document.querySelector('.meio_titulo');
+    const imagemMeio = document.querySelector('.meio_imagem');
+
+    const elementoCompletoDireita = document.querySelector('.carrosel_esquerda');
+    const linkDireita = document.querySelector('.direita_link');
+    const imagemDireita = document.querySelector('.direita_imagem');
+    if(indice>0){
+        indice--
+        imagemMeio.setAttribute('src',`${dadosBanco[indice].imagem}`);
+        linkMeio.href = `${dadosBanco[indice].url}`; 
+        tituloMeio.innerHTML = `${dadosBanco[indice].titulo}`;
+        descricaoMeio.innerHTML = `${dadosBanco[indice].descricao}`;
+
+        if(indice>=1 && indice<dadosBanco.length-1){
+            let esquerda = indice;
+            esquerda--
+            let direita = indice
+            direita++
+            imagemDireita.setAttribute('src',`${dadosBanco[direita].imagem}`);
+            linkDireita.href = `${dadosBanco[direita].url}`; 
+
+            imagemEsquerda.setAttribute('src',`${dadosBanco[esquerda].imagem}`);
+            linkEsquerda.href = `${dadosBanco[esquerda].url}`; 
+        } else if(indice==0) {
+            imagemDireita.setAttribute('src',`${dadosBanco[1].imagem}`);
+            linkDireita.href = `${dadosBanco[1].url}`; 
+
+            imagemEsquerda.setAttribute('src',`${dadosBanco[dadosBanco.length-1].imagem}`);
+            linkEsquerda.href = `${dadosBanco[dadosBanco.length-1].url}`; 
+        }
+        console.log(indice)
+        
+    }else if(indice===0){
+        indice = dadosBanco.length-1;
+        imagemMeio.setAttribute('src',`${dadosBanco[indice].imagem}`);
+        linkMeio.href = `${dadosBanco[indice].url}`; 
+        tituloMeio.innerHTML = `${dadosBanco[indice].titulo}`;
+        descricaoMeio.innerHTML = `${dadosBanco[indice].descricao}`;
+
+
+        imagemDireita.setAttribute('src',`${dadosBanco[dadosBanco.length-3].imagem}`);
+        linkDireita.href = `${dadosBanco[dadosBanco.length-3].url}`; 
+
+        imagemEsquerda.setAttribute('src',`${dadosBanco[dadosBanco.length-2].imagem}`);
+        linkEsquerda.href = `${dadosBanco[dadosBanco.length-2].url}`; 
+        
+
+    }
+    
 }
+const btnRetornar = document.querySelector('.button_retornar');
+btnRetornar.addEventListener('click', ()=>{
+    carroselRetorcer();
+})
