d0268ea5a78809a10cf157cfbf481acb6628bec3
Amandalfs <caduc57@gmail.com>
2023-03-08 23:55:53 -0300

    feat
    Realizado o banco de dados dos projetos pessoais

---
+++ assets/javascript/database.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+const dadosBanco = [
+    {
+        imagem: "assets/imagens_projetos/loteria.png",
+        titulo: "loteria",
+        descricao: "Loteria com 5 numeros",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/jogodaforca.png",
+        titulo: "jogo da forca",
+        descricao: "Jogo da forca que tem dois temas",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/nutricionista.png",
+        titulo: "nutricionista",
+        descricao: "site de nutricao",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/jogodaforca.png",
+        titulo: "Joken por",
+        descricao: "Jogo de joken por",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/conversor.png",
+        titulo: "Convertor binario",
+        descricao: "conertor de numero binario para decimal",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/bancohzhong.png",
+        titulo: "Hzhong",
+        descricao: "banco ficticio",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/newlabsWryzen.png",
+        titulo: "NewLabs Wryzen",
+        descricao: "E-commerme de pc e acessorios ficticio",
+        url: "",
+    },
+    {
+        imagem: "assets/imagens_projetos/sitedoce.png",
+        titulo: "Site de doces",
+        descricao: "E-commer de doces ficticios",
+        url: "",
+    },
+]
+
+module.exports = dadosBanco;
